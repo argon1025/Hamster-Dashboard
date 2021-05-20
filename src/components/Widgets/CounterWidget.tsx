72467d72cbbd5ecd3949c00c116236ceb8d407a7
argon1025 <argon1025@gmail.com>
2021-05-20 15:23:48 +0900

    Add 로그 시스템 컴포넌트 추가, 카운터 위젯 디자인 변경 #9

--- src/components/Widgets/CounterWidget.tsx
+++ src/components/Widgets/CounterWidget.tsx
@@ -8,11 +8,11 @@ interface CounterWidgetProps {
 class CounterWidget extends React.Component<CounterWidgetProps> {
   render() {
     return (
-      <div className="w-full lg:w-1/5 rounded-lg overflow-hidden shadow-md m-3">
-        <div className="widget w-full p-4 rounded-lg bg-white border-l-4 border-purple-400">
+      <div className="w-full lg:w-1/5 rounded-lg shadow-md mr-3">
+        <div className="widget w-full h-full p-4 rounded-lg bg-white border-l-4 border-blue-500">
           <div className="flex items-center">
-            <div className="icon w-14 p-3.5 bg-purple-400 text-white rounded-full mr-3">
-              <svg fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <div className="icon w-14 p-3.5 bg-gradient-to-r from-green-300 to-blue-500 text-white rounded-full mr-3">
+              <svg className="" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path
                   stroke-linecap="round"
                   stroke-linejoin="round"
